54831d723505b17e37da244a98d2c6801940e4b6
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-04-07 15:26:07 -0300

    feat: closing the connections

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/rafaeldepontes/goplo/internal/handler"
+	"github.com/rafaeldepontes/goplo/pkg/cache"
+	"github.com/rafaeldepontes/goplo/pkg/db/postgres"
 )
 
+func init() {
+	_ = godotenv.Load(".env", ".env.example")
+	cache.GetCache()
+	postgres.GetDb()
+}
+
 func main() {
+	defer cache.Close()
+	defer postgres.Close()
+
 	h := handler.NewHandler()
 
-	log.Fatalln(http.ListenAndServe(":8080", h))
+	log.Fatalln(http.ListenAndServe(":"+os.Getenv("API_PORT"), h))
 }
