a0c0d4030e11e867b341d4a4e7fd5516aac4d4e7
rafaldepontes <rafael.cr.carneiro@gmail.com>
2026-04-08 19:06:59 -0300

    Revert "feat: closing the connections"
    This reverts commit 54831d723505b17e37da244a98d2c6801940e4b6.

--- ledger/main.go
+++ ledger/main.go
@@ -3,25 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/rafaeldepontes/goplo/internal/handler"
-	"github.com/rafaeldepontes/goplo/pkg/cache"
-	"github.com/rafaeldepontes/goplo/pkg/db/postgres"
 )
 
-func init() {
-	_ = godotenv.Load(".env", ".env.example")
-	cache.GetCache()
-	postgres.GetDb()
-}
-
 func main() {
-	defer cache.Close()
-	defer postgres.Close()
-
 	h := handler.NewHandler()
 
-	log.Fatalln(http.ListenAndServe(":"+os.Getenv("API_PORT"), h))
+	log.Fatalln(http.ListenAndServe(":8080", h))
 }
