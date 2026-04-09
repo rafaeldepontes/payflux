89fe67df51832b737cbd31546f92bdd4e1fe0f05
rafaldepontes <rafael.cr.carneiro@gmail.com>
2026-04-08 21:30:15 -0300

    feat: calling migration on start up

--- ledger/main.go
+++ ledger/main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/rafaeldepontes/goplo/internal/handler"
+	"github.com/rafaeldepontes/goplo/pkg/cache"
+	"github.com/rafaeldepontes/goplo/pkg/db/postgres"
 )
 
+func init() {
+	_ = godotenv.Load(".env", ".env.example")
+	cache.GetCache()
+	postgres.GetDb()
+	if err := postgres.RunMigrations(); err != nil {
+		log.Fatalf("failed to run migrations: %v", err)
+	}
+}
+
 func main() {
+	defer cache.Close()
+	defer postgres.Close()
+
+	port := os.Getenv("API_PORT")
+
 	h := handler.NewHandler()
 
-	log.Fatalln(http.ListenAndServe(":8080", h))
+	log.Println("Application running on localhost:" + port)
+	log.Fatalln(http.ListenAndServe(":"+port, h))
 }
